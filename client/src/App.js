75d2243b0bca73e1c7999566003f281ef3255c0d
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2022-12-20 08:02:16 +0300

    init

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <nav>
+                    <ul>
+                        <li>Главная</li>
+                        <li>Эскизы</li>
+                        <li>Попечители</li>
+                        <li>Контакты</li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    );
+}
+
+export default App;
